6485e4b885f51f34d58296b5dd37ddf6b16c5979
Lasse Kokholm <36420523+aegisigea@users.noreply.github.com>
2023-02-12 22:16:23 +0100

    Split configuration functions into different files

---
+++ user/config.el
@@ -0,0 +1,9 @@
+;; -*- mode: emacs-lisp; lexical-binding: t -*-
+
+(defun dotspacemacs/user-config ()
+  "Configuration for user code:
+This function is called at the very end of Spacemacs startup, after layer
+configuration.
+Put your configuration code here, except for variables that should be set
+before packages are loaded."
+)
