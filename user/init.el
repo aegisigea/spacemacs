3a0dfc036689eb0f3e0ba8357e5acffd9bf47997
Lasse Kokholm <36420523+aegisigea@users.noreply.github.com>
2023-02-14 21:51:21 +0100

    Fix custom file not being assigned a proper file extension

--- user/init.el
+++ user/init.el
@@ -7,6 +7,6 @@ configuration.
 It is mostly for variables that should be set before packages are loaded.
 If you are unsure, try setting them in `dotspacemacs/user-config' first."
   ;; Define custom file for storing automatically generated custom settings.
-  (setq custom-file "~/.spacemacs.d/.custom")
+  (setq custom-file "~/.spacemacs.d/.custom.el")
   (load custom-file)
 )
